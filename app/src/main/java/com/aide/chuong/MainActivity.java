e51b706b98bcc2befd3d8f3368fabae4548d3292
DangChuVM <akirahung905@gmail.com>
2025-08-19 08:34:04 +0700

    Initial commit

---
+++ app/src/main/java/com/aide/chuong/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aide.chuong;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.aide.chuong.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private long pressedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View _view) {
+                        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            finishAffinity();
+        } else {
+            Toast.makeText(getBaseContext(), "Nhấn lại lần nữa để thoát", Toast.LENGTH_SHORT)
+                    .show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
